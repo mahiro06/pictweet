d74762d8bc4234944ba1188d2c454639c744944d
mahiro06 <shishamo06@gmail.com>
2022-04-11 15:54:36 +0900

    サーチ機能の記述をサービスクラスへ変更

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,12 +4,4 @@ class Tweet < ApplicationRecord
   has_many :comments
   # validates :user_id presence: true
 
-  def self.search(search)
-    if search != ""
-      Tweet.where('text LIKE(?)', "%#{search}%")
-    else
-     Tweet.all
-    end
-  end
 end
-
